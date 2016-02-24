9e6040006e247ff7b52297b72d00e0976413019b
ancymathews <ansamma.mathews@blueshieldca.com>
2016-02-24 14:41:59 -0800

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,16 +1,17 @@
 class users::admins {
-users::managed_user { 'joe': 
-home_base = "/tmp",
-}
-users::managed_user { 'alice':
-home_base = "/tmp",
-group => 'staff',
-}
-users::managed_user { 'aaron':
-home_base = "/tmp",
-group => 'staff',
-}
-
-group { 'staff':
-ensure => present,
+  users::managed_user { 'joe': 
+  home_base = "/tmp",
+  }
+  users::managed_user { 'alice':
+  home_base = "/tmp",
+  group => 'staff',
+  }
+  users::managed_user { 'aaron':
+  home_base = "/tmp",
+  group => 'staff',
+  }
+  
+  group { 'staff':
+  ensure => present,
+  }
 }
