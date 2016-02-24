8d403c20d6b253d0389f07827e564201cc84e618
ancymathews <ansamma.mathews@blueshieldca.com>
2016-02-24 14:26:21 -0800

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,21 @@
+define users::managed_user (
+$home_base = "/home".
+$group = $title,
+) {
+
+  File{
+    owner=> $title,
+    group => $title,
+    mode=> '0755',
+  }
+
+  user { $title:
+  ensure => present,
+  }
+
+  file { "$home_base/${title}":
+  ensure => directory,
+  owner => $title,
+  group => $group,
+  }
+}
