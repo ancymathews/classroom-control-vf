97d6daee573d8074d7c89402feb219bbf045d597
ancymathews <ansamma.mathews@blueshieldca.com>
2016-02-24 14:32:40 -0800

    Create admins.pp

---
+++ site/users/manifests/admins.pp
@@ -0,0 +1,16 @@
+class users::admins {
+users::managed_user { 'joe': 
+$home_base = "/tmp",
+}
+users::managed_user { 'alice':
+$home_base = "/tmp",
+group => 'staff',
+}
+users::managed_user { 'aaron':
+$home_base = "/tmp",
+group => 'staff',
+}
+
+group { 'staff':
+ensure => present,
+}
