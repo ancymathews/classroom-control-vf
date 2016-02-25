77d3f9da0e64878ca357d3a8ad68bca8d3e3e7c9
ancymathews <ansamma.mathews@blueshieldca.com>
2016-02-25 09:47:22 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,9 +42,9 @@ node default {
 # This is where you can declare classes for all nodes.
 # Example:
 # class { 'my_class': }
-notify { "Hello, my name is ${::hostname}": }
+# notify { "Hello, my name is ${::hostname}": }
 # include users::admins
 include nginx
 $message = hiera('message')
-notify { $message: }
+notify { "$message": }
 }
