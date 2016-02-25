b1a08b82a802f0f18505a1ea527c5f31aaea0b49
ancymathews <ansamma.mathews@blueshieldca.com>
2016-02-25 13:31:35 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,7 +44,7 @@ node default {
 # class { 'my_class': }
 # notify { "Hello, my name is ${::hostname}": }
 # include users::admins
-include nginx
+#include nginx
 
 $message = hiera('nginx::docroot')
 notify { "$message": }
