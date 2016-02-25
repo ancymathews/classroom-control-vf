92ddd71fc0c950a8767a76861c04618e76358584
ancymathews <ansamma.mathews@blueshieldca.com>
2016-02-25 11:52:05 -0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,45 +1,51 @@
 class nginx (
-$owner = $nginx::params::owner,
-$group = $nginx::params::group,
-$docroot = $nginx::params::docroot,
-$confdir = $nginx::params::confdir,
-$logdir = $nginx::params::logdir,
-$user = $nginx::params::user,
-) inherits nginx::params {
+      $owner = $nginx::params::owner,
+      $group = $nginx::params::group,
+      $docroot = $nginx::params::docroot,
+      $confdir = $nginx::params::confdir,
+      $logdir = $nginx::params::logdir,
+      $user = $nginx::params::user,
+      ) inherits nginx::params {
+/*
+      $docroot = $root ? {
+            undef => $docroot,
+            default => $root,
+      }
+*/
 
-$docroot = $root ? {
-      undef => $default_docroot,
-      default => $root,
-}
-
-
-File {
-owner => $owner,
-group => $group,
-mode => '0664',
-}
-package { $package:
-ensure => present,
-}
-file { [ $docroot, "${confdir}/conf.d" ]:
-ensure => directory,
-}
-file { "${docroot}/index.html":
-ensure => file,
-source => 'puppet:///modules/nginx/index.html',
-}
-file { "${confdir}/nginx.conf":
-ensure => file,
-content => template('nginx/nginx.conf.erb'),
-notify => Service['nginx'],
-}
-file { "${confdir}/conf.d/default.conf":
-ensure => file,
-content => template('nginx/default.conf.erb'),
-notify => Service['nginx'],
-}
-service { 'nginx':
-ensure => running,
-enable => true,
-}
+      File {
+      owner => $owner,
+      group => $group,
+      mode => '0664',
+      }
+      
+      package { $package:
+      ensure => present,
+      }
+      
+      file { [ $docroot, "${confdir}/conf.d" ]:
+      ensure => directory,
+      }
+      
+      file { "${docroot}/index.html":
+      ensure => file,
+      source => 'puppet:///modules/nginx/index.html',
+      }
+      
+      file { "${confdir}/nginx.conf":
+      ensure => file,
+      content => template('nginx/nginx.conf.erb'),
+      notify => Service['nginx'],
+      }
+      
+      file { "${confdir}/conf.d/default.conf":
+      ensure => file,
+      content => template('nginx/default.conf.erb'),
+      notify => Service['nginx'],
+      }
+      
+      service { 'nginx':
+      ensure => running,
+      enable => true,
+      }
 }
